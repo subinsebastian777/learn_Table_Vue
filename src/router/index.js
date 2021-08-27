dd7b2b2cc4c17003a60fbcba4cfa265b153e52f9
subin sebastian <subinsebastian777@gmail.com>
2021-08-27 13:49:04 -0400

    firstCommit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import PetTable from '../views/PetTable.vue'
+import Pets from '../views/Pets.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/PetTable',
+    name: 'PetTable',
+    component: PetTable
+  },
+  {
+    path: '/Pet',
+    name: 'Pet',
+    component: Pets
   }
 ]
 
---
+++ src/views/PetTable.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <b-table :fields="fields" :items="items">
+      <template #cell(name)="data">
+        <!-- `data.value` is the value after formatted by the Formatter -->
+        <a :href="`#${data.value.replace(/[^a-z]+/i,'-').toLowerCase()}`">{{ data.value }}</a>
+      </template>
+    </b-table>
+  </div>
+</template>
